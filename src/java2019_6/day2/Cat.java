eabf93aab2ff95b862245969550f1e9813c1a283
devil <183295049@qq.com>
2019-06-18 11:42:57 +0800

    2019.6.18 day2

--- src/java2019_6/day2/Cat.java
+++ src/java2019_6/day2/Cat.java
@@ -3,16 +3,27 @@ package java2019_6.day2;
 public class Cat {
 
     public Cat(){
-        System.out.println("cat对象创建");
         age = 5;
         gender = "雄";
         color = "白";
     }
 
-    public Cat(int age,String gender,String color){
+    public Cat(int age, String gender, String color){
         this.color = color;
         this.gender = gender;
         this.age = age;
+    }
+
+    public Cat(String gender, String color) {
+        super();
+        this.gender = gender;
+        this.color = color;
+    }
+
+    public Cat(int age, String color) {
+        super();
+        this.age = age;
+        this.color = color;
     }
 
     int age;
@@ -21,5 +32,12 @@ public class Cat {
 
     public void eat(){
         System.out.println("吃");
+    }
+
+    public void eat(String food){
+        System.out.println("吃"+food);
+    }
+    public void eat(int number){
+        System.out.println("吃"+number);
     }
 }
---
+++ src/java2019_6/day2/Demo4.java
@@ -0,0 +1,11 @@
+package java2019_6.day2;
+
+public class Demo4 {
+    public static void main(String[] args) {
+
+        Cat cat1 = new Cat();
+        cat1.eat(12);
+        cat1.eat("鱼");
+
+    }
+}
