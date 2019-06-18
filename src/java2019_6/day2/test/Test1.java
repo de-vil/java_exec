90f1188cf67dd966021fd049375e11737b7bccf0
devil <183295049@qq.com>
2019-06-18 11:57:02 +0800

    2019.6.18 day2 one

--- src/java2019_6/day2/test/Test1.java
+++ src/java2019_6/day2/test/Test1.java
@@ -3,6 +3,7 @@ package java2019_6.day2.test;
 public class Test1 {
     public static void main(String[] args) {
         Computer computer = new Computer("i9 9900k","16G","1t","2080ti");
+        computer.cpu = "i3 8100";
         System.out.println(computer);
     }
 }
