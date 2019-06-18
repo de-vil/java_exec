5c03bb8a27c9a51dacf8e85c87a71e48763dd4fd
devil <183295049@qq.com>
2019-06-18 11:55:54 +0800

    2019.6.18 day2 one

---
+++ src/java2019_6/day2/test/Test1.java
@@ -0,0 +1,8 @@
+package java2019_6.day2.test;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Computer computer = new Computer("i9 9900k","16G","1t","2080ti");
+        System.out.println(computer);
+    }
+}
